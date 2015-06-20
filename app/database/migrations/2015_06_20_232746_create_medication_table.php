642031a3516ae0056fa185c0912c92338045fe2d
Nathan Disidore <ndisidore@gmail.com>
2015-06-20 19:46:57 -0400

    Add medications database schema

--- app/database/migrations/2015_06_20_232746_create_medication_table.php
+++ app/database/migrations/2015_06_20_232746_create_medication_table.php
@@ -13,6 +13,16 @@ class CreateMedicationTable extends Migration {
 	public function up()
 	{
 		//
+		Schema::create('medications', function($table) {
+			$table->increments('id');
+			$table->integer('uid');
+			$table->string('name');
+			$table->smallInteger('dosage_volume');
+			$table->string('dosage_unit');
+			$table->string('frequency');
+			$table->smallInteger('count');
+			$table->string('ndc', 15);
+		} );
 	}
 
 	/**
@@ -22,7 +32,7 @@ class CreateMedicationTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::dropIfExists('medications');
 	}
 
 }
