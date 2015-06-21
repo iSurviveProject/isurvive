a27264bb9465f5e98222d23a9a7fd5b915e96dc9
Souleymane Coulibaly <bina.coulibaly@gmail.com>
2015-06-21 09:47:36 -0400

    Google chart test 2

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -25,7 +25,7 @@
             vAxis: {title: 'Levels', minValue: 0, maxVlaue: 10000}
           };
 
-          var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+          var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
           chart.draw(data, options);
         }
     </script>
