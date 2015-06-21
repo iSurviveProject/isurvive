12f708cefd5131fecae33899a300e7a95fd8b476
Nathan Disidore <ndisidore@gmail.com>
2015-06-20 22:33:35 -0400

    Fleshing out Medication Controller, Model and View

--- app/controllers/MedicationController.php
+++ app/controllers/MedicationController.php
@@ -2,9 +2,20 @@
 
 class MedicationController extends BaseController {
 
-	public function index(){}
-	public function show(){}
-	public function create(){}
+	public function index(){
+		$meds = Medication::all();
+		return View::make('medication.list')->with('medications', $meds);
+	}
+	public function show($id){
+		$user_meds = Medication::find($id);
+
+		return View::make('medication', array('medications' => $user_meds));
+	}
+	public function create()
+	{
+		// load the create form (app/views/medication/create.blade.php)
+		return View::make('medication.create');
+	}
 	public function store(){}
 	public function edit(){}
 	public function update(){}
