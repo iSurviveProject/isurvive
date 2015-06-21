2b86e1c6cc4435dc02d7580d4587475a72b7aa72
Nathan Disidore <ndisidore@gmail.com>
2015-06-21 11:04:43 -0400

    Fix medication link, add gross but hopefully functionaly drug-info endpoint

--- app/views/medication/list.blade.php
+++ app/views/medication/list.blade.php
@@ -31,7 +31,7 @@
                         <tr>
                             <td>{{ $meds->name }}</td>
                             <td>{{ $meds->dosage }} {{ $meds->dosage_unit }}</td>
-                            <td>{{ HTML::link('user/edit/'.$meds->uid,'Edit', array('class' => 'btn btn-xs btn-warning')) }} {{ HTML::link('user/delete/'.$meds->uid, 'Delete', array('class' => 'btn btn-xs btn-danger')) }}</td>
+                            <td>{{ HTML::link('medication/edit/'.$meds->uid,'Edit', array('class' => 'btn btn-xs btn-warning')) }} {{ HTML::link('medication/delete/'.$meds->uid, 'Delete', array('class' => 'btn btn-xs btn-danger')) }}</td>
                         </tr>
                     @endforeach
                     </tbody>
