10618a2057c467373142f2ca3b11f53ff32f878c
Souleymane Coulibaly <bina.coulibaly@gmail.com>
2015-06-21 09:44:20 -0400

    Google chart test

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -2,37 +2,33 @@
 @section('content')
 <!-- Jumbotron Header -->
 <header class="jumbotron hero-spacer">
-    <h1>CD4 Levels</h1>
+    <h1>CD4 Levels <a href="/cd4" class="btn btn-primary btn-large">More Details</a></h1>
     <div id="cd4-level-graph"></div>
     <script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1.1','packages':['corechart']}]}"></script>
-    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+    <div id="chart_div"></div>
     <script type="text/javascript">
         google.setOnLoadCallback(drawVisualization);
 
         function drawVisualization() {
           // Some raw data (not necessarily accurate)
           var data = google.visualization.arrayToDataTable([
-            ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-            ['2004/05',  165,      938,         522,             998,           450,      614.6],
-            ['2005/06',  135,      1120,        599,             1268,          288,      682],
-            ['2006/07',  157,      1167,        587,             807,           397,      623],
-            ['2007/08',  139,      1110,        615,             968,           215,      609.4],
-            ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+            ['Month', 'Levels', 'Optimal'],
+            ['2004/05',  2500,      1600],
+            ['2005/06',  1800,      1600],
+            ['2006/07',  3050,      1600],
+            ['2007/08',  3300,      1600],
+            ['2008/09',  4000,      1600]
           ]);
 
           var options = {
-            title : 'Monthly Coffee Production by Country',
-            vAxis: {title: "Cups"},
-            hAxis: {title: "Month"},
-            seriesType: "function",
-            series: {5: {type: "line"}}
+            hAxis: {title: 'Year'},
+            vAxis: {title: 'Levels', minValue: 0, maxVlaue: 10000}
           };
 
           var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
           chart.draw(data, options);
         }
     </script>
-    <p><a href="/cd4" class="btn btn-primary btn-large">More Details</a></p>
 </header>
 
 <!-- Title -->
