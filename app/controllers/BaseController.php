2562204968aaaaef9367141df9d45b13a60781cf
Souleymane Coulibaly <bina.coulibaly@gmail.com>
2015-06-21 13:49:30 -0400

    Merge 6

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -15,7 +15,7 @@ class BaseController extends Controller {
 
 	public function dashboard() {
         $cd4 = Cd4::where('uid', Auth::id())->get();
-        $cd4 = $cd4::orderBy('date', 'desc');
+        $cd4 = $cd4->orderBy('date', 'desc')->get();
 
         $graphData = array(array('Month', 'Levels'));
 
--- app/controllers/Cd4Controller.php
+++ app/controllers/Cd4Controller.php
@@ -5,7 +5,7 @@ class Cd4Controller extends BaseController {
 	public function index(){
         //if( Auth::check() ) {
             $cd4 = Cd4::where('uid', Auth::id())->get();
-            $cd4 = $cd4::orderBy('date', 'desc');
+            $cd4 = $cd4->orderBy('date', 'desc')->get();
             return View::make('cd4.show')->with('cd4', $cd4);
         //}else{
             //return Redirect::to('login');
