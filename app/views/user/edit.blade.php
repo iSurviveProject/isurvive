a2eecd381d3b5fe0d4c63baf3c47c70e0bbff99a
Drethic <drethic@msn.com>
2015-06-21 10:06:36 -0400

    Adding more debug to edit blade

--- app/views/user/edit.blade.php
+++ app/views/user/edit.blade.php
@@ -26,7 +26,7 @@
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="username">Username</label>
-                    <input type="text" class="form-control" name="username" id="username" value="{{ $user->username }}">
+                    
                 </div>
                 <hr>
                 <p>If you would like to change this accounts password you may include new passwords below. This is not required to edit an account</p>
