7f592f5bdae26dca55ac3dd861e12483ae7a57d6
Souleymane Coulibaly <bina.coulibaly@gmail.com>
2015-06-21 10:28:11 -0400

    test cd4 save 6

--- app/controllers/Cd4Controller.php
+++ app/controllers/Cd4Controller.php
@@ -13,7 +13,6 @@ class Cd4Controller extends BaseController {
 	public function store(){
 		$cd4 = new Cd4();
 		$cd4->level = Input::get('level');
-
 		$date = date('Y-m-d H:i:s', strtotime( Input::get('date')));
 		$cd4->date = $date;
 		$cd4->save();
