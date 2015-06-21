bf6af559c48ca2d5ae854275628c7df81a3385b6
Nathan Disidore <ndisidore@gmail.com>
2015-06-21 08:42:42 -0400

    Continue fleshing out Medication contoller and blades

--- app/views/medication/create.blade.php
+++ app/views/medication/create.blade.php
@@ -1,23 +1,11 @@
 <!-- app/views/medication/create.blade.php -->
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Look! I'm CRUDding</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-</head>
-<body>
-<div class="container">
-
-    <nav class="navbar navbar-inverse">
-        <ul class="nav navbar-nav">
-            <li><a href="{{ URL::to('medication') }}">View All Medications</a></li>
-            <li><a href="{{ URL::to('medication/create') }}">+ New Medication</a>
-        </ul>
-    </nav>
-
+@extends('app')
+@section('content')
+<div class="page-header">
     <h1>Add a Medication</h1>
-
+</div>
+<div class="panel panel-default">
     <!-- if there are creation errors, they will show here -->
     {{ HTML::ul($errors->all()) }}
 
@@ -40,7 +28,7 @@
 
     <div class="form-group">
         {{ Form::label('frequency', 'Frequency') }}
-        {{ Form::times('frequency', Input::old('frequency'), array('class' => 'form-control')) }}
+        {{ Form::number('frequency', Input::old('frequency'), array('class' => 'form-control')) }}
     </div>
 
     <div class="form-group">
@@ -53,10 +41,11 @@
         {{ Form::number('count', Input::old('count'), array('class' => 'form-control')) }}
     </div>
 
+    {{ Form::hidden('ndc', Input::old('ndc')) }}
+
     {{ Form::submit('Add Medication!', array('class' => 'btn btn-primary')) }}
 
     {{ Form::close() }}
 
 </div>
-</body>
-</html>
+@endsection
