f7e6c7c6e2fc6cc0d8e20ff31b55099d56745ced
Drethic <drethic@msn.com>
2015-06-21 12:02:04 -0400

    Masked user edit with profile pt 2.

--- app/routes.php
+++ app/routes.php
@@ -20,7 +20,7 @@ Route::get('/', array('uses' => 'BaseController@dashboard'));
 //Route::model('user', 'User');
 Route::model('setting', 'Setting');
 Route::get('profile', function(){
-	return User::edit(Auth::id());
+	return UserController::edit(Auth::id());
 });
 Route::resource('medication', 'MedicationController');
 Route::resource('cd4', 'Cd4Controller');
