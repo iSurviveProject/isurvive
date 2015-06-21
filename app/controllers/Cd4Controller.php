64025c9f94366f09e69b8e6938295c77f89be4f8
Drethic <drethic@msn.com>
2015-06-21 13:36:06 -0400

    Better filtering on cd4 values

--- app/controllers/Cd4Controller.php
+++ app/controllers/Cd4Controller.php
@@ -4,7 +4,7 @@ class Cd4Controller extends BaseController {
 
 	public function index(){
         //if( Auth::check() ) {
-            $cd4 = Cd4::orderBy('date', 'desc')->get();
+            $cd4 = Cd4::orderBy('date', 'desc')->where('user.uid', Auth::id())->get();
             return View::make('cd4.show')->with('cd4', $cd4);
         //}else{
             //return Redirect::to('login');
