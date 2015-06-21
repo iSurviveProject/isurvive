b6240a423cf38f7bb868d8da735ea352971915d0
Drethic <drethic@msn.com>
2015-06-21 12:24:35 -0400

    Profile debug pt 2

--- app/models/Profile.php
+++ app/models/Profile.php
@@ -2,9 +2,10 @@
 
 class Profile extends Eloquent {
 	protected $table = 'profiles';
-	
+	protected $primaryKey = 'uid';
+
 	public function user()
 	{
-		return $this->belongsTo('User', 'uid', 'uid');
+		return $this->belongsTo('User', 'uid');
 	}
 }
