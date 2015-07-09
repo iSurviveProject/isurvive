5f62e6bfab159132c299cbd90f0b6d05f5b4957c
Joseph Robinson <drethic@msn.com>
2015-07-09 17:31:12 +0000

    Initial commit with Laravel 5 and Bootstrap CMS.  Modified config/database.php, config/mail.php, and config/credentials.php to account for c9.io web IDE.  Original code left commented above.  To Do, add comments of what was commented out.

---
+++ config/credentials.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of Bootstrap CMS.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Public Registration
+    |--------------------------------------------------------------------------
+    |
+    | This defines if public registration is allowed.
+    |
+    | Default to true.
+    |
+    */
+
+    'regallowed' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Require Account Activation
+    |--------------------------------------------------------------------------
+    |
+    | This defines if public registration requires email activation.
+    |
+    | Default to true.
+    |
+    */
+
+    //'activation' => true,
+    'activation' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Revision Model
+    |--------------------------------------------------------------------------
+    |
+    | This defines the revision model to be used.
+    |
+    | Default: 'GrahamCampbell\Credentials\Models\Revision'
+    |
+    */
+
+    'revision' => 'GrahamCampbell\Credentials\Models\Revision',
+
+];
